35f56434da9cc71a4e71fa4e837830e6a031f451
St10278399im <st10278399@imconnect.edu.za>
2025-10-07 21:48:44 +0200

    CLDV  PART 2 submission
    Cldv Part two with Azure function

---
+++ CLDV Attempt/TestQueueNew/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml.Linq;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+
+namespace TestQueueNew
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            // Your real Azure Storage connection string
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=adeolastore;AccountKey=9kRIriHodafBJe+0wjhTUmpVZwONaFgVr3pnTaGbR/KJYVxoXZSBUuQ9s0a6mvk64o/TXHWgJUVH+AStblsg8g==;EndpointSuffix=core.windows.net";
+            // Queue name must match the one your function listens to
+            var queueClient = new QueueClient(
+                connectionString,
+                "retailque",
+                new QueueClientOptions { MessageEncoding = QueueMessageEncoding.Base64 } //ensure its base64
+            );
+            // Create queue if it doesn't exist
+            await queueClient.CreateIfNotExistsAsync();
+            // Build test object
+
+            var person = new { Name = "Wandile Smith", Email = "wandile@iiemsa.com" };
+            // Serialize object to JSON
+            string json = JsonSerializer.Serialize(person);
+            // Send as plain JSON string
+            await queueClient.SendMessageAsync(json);
+            Console.WriteLine($"Message sent: {json}");
+        }
+    }
+}
